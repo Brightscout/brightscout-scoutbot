43bd5cfca3af8eca677c36f329888c7147b4bcd1
Michael DeBonis <mike@devised.io>
2018-01-06 21:02:52 -0600

    Fixing linting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
   This is the programmatic entry point of your bot.
   Your bot's logic resides here.
-  
+
   Here's the next steps for you:
   1. Read this file to understand how this simple bot works
   2. Read the `content.yml` file to understand how messages are sent
@@ -20,20 +20,28 @@
 */
 
 module.exports = function(bp) {
+  //
   // Listens for a first message (this is a Regex)
   // GET_STARTED is the first message you get on Facebook Messenger
+  //
   bp.hear(/GET_STARTED|hello|hi|test|hey|holla/i, (event, next) => {
     event.reply('#welcome') // See the file `content.yml` to see the block
   })
-
+  //
   // You can also pass a matcher object to better filter events
-  bp.hear({
-    type: /message|text/i,
-    text: /exit|bye|goodbye|quit|done|leave|stop/i
-  }, (event, next) => {
-    event.reply('#goodbye', {
-      // You can pass data to the UMM bloc!
-      reason: 'unknown'
-    })
-  })
+  //
+  bp.hear(
+    {
+      type: /message|text/i,
+      text: /exit|bye|goodbye|quit|done|leave|stop/i
+    },
+    (event, next) => {
+      event.reply('#goodbye', {
+        //
+        // You can pass data to the UMM bloc!
+        //
+        reason: 'unknown'
+      })
+    }
+  )
 }
